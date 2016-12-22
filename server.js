889ce898f3b7730cc6fe0419d20db0a95f5611c7
Hasun Khan <hasunkhan26@gmail.com>
2016-12-22 16:03:42 -0500

    change second mistake redirecting https to http

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 9005
 app.use(function(req,res,next){
-  if(req.headers['x-forwareded-proto']==='http'){
+  if(req.headers['x-forwarded-proto']==='http'){
     next();
   } else {
     res.redirect('http://'+req.hostname+req.url);
