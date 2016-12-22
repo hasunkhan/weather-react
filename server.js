e27d82f8bb892a7605d95ea6cd1d55eaafc3ffb4
Hasun Khan <hasunkhan26@gmail.com>
2016-12-22 15:42:08 -0500

    change server for heroku

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 // Create our app
 var app = express();
+var port = process.env.PORT || 9005
+
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(port, function () {
+  console.log(`Express server is up on port 3000: ${port}`);
 });
